911c90bf211d542f4bf63a029f80c2f98a3fc2f6
gabe thomas <gabe.thomas@bizzabo.com>
2021-07-06 20:27:09 -0400

    pushing minor changes

---
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./style.css";
+
+function Jumbotron() {
+    return (
+        <div>
+            <div className="jumbotron">
+                <h1 className="display-4 text-center myText">Google Book Search React</h1>
+                <br></br>
+                <h2 className="text-center myText">Try searching for your favorite book!</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Jumbotron;
